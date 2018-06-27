0c10d146fdada02fead31161421bcd7fc8571a48
parkchansoo <chansoo1563@gmail.com>
2018-06-27 15:11:30 +0900

    stack env and typo correction

---
+++ data_structure_CS202_2017f/myStack_largestRectangleInHistogram/src/myStack/Main.java
@@ -0,0 +1,17 @@
+package myStack;
+
+public class Main {
+    public static void main(String[] args) {
+        // give test cases
+    }
+
+    public static String printTest(String Eans, String Oans) {
+        String ret = "";
+        return ret;
+    }
+
+    public static String getLargestRectArea(double[] histogram) {
+        String ret = "";
+        return ret;
+    }
+}
---
+++ data_structure_CS202_2017f/myStack_largestRectangleInHistogram/src/myStack/myStack.java
@@ -0,0 +1,10 @@
+package myStack;
+
+public class myStack {
+    /* myStack properties
+     * variables : stack pointer, array to store
+     * stack functions : constructor, get/set Size, push, pop, top, isEmpty
+     */
+
+
+}
