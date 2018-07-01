b3ebba6b586e5a871f8a26e708bbe1f1e6be5d6d
parkchansoo <chansoo1563@gmail.com>
2018-07-01 23:06:36 +0900

    chess game template

---
+++ data_structure_CS202_2017f/chessGame/src/chessGame/main.java
@@ -0,0 +1,12 @@
+package chessGame;
+
+public class main {
+    public static void main(String argv) {
+
+    }
+
+    public static int minimunKnightMove(int[] startPos, int[] targetPos) {
+        int ret = 0;
+        return ret;
+    }
+}
---
+++ data_structure_CS202_2017f/chessGame/src/chessGame/myQueue.java
@@ -0,0 +1,11 @@
+package chessGame;
+
+public class myQueue {
+    /*
+    * myQueue AST
+    * 1. variables : last, size, maxSize
+    * 2. methods : getSize, set/getFirst, set/getLast, enqueue, dequeue,
+    * 3. helper functions :
+    * 4. functions for chess game
+    * */
+}
